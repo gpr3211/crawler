86648fc33731eb09387ecb0e13d2f61955f354f5
Pavlin <pavling.georgiev@gmail.com>
2024-09-22 01:16:55 -0400

    6.
    added cli arg

--- main.go
+++ main.go
@@ -2,10 +2,23 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+
+	args := os.Args
+	if len(args) < 2 {
+		fmt.Println("no website provided")
+		os.Exit(1)
+	} else if len(args) > 2 {
+		fmt.Println("too many arguments provided")
+		os.Exit(1)
+	}
+
+	inp := args[1]
+	fmt.Println("starting crawl: ", inp)
+
 	return
 
 }
